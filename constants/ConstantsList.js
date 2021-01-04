a17ae407da3094a311346c3e5e0bd6965051aaf8
andmorefine <andmorefine@gmail.com>
2021-01-05 02:40:57 +0900

    fix proverbank

--- constants/ConstantsList.js
+++ constants/ConstantsList.js
@@ -1,4 +1,4 @@
 module.exports = Object.freeze({
-  END_POINT:'http://localhost:3001/api/since',
+  END_POINT:'https://bank.andmorefine.com/api/since',
   IMAGE_URL:'https://d2y9vn9mruehfd.cloudfront.net/img/proverb',
 })
